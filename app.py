a7f0d85679942dd2b3a7fd55d428e3a158eebf08
Yonatan365 <flyingjony@gmail.com>
2025-06-17 11:35:31 +0300

    changed to assign dynamic port

--- app.py
+++ app.py
@@ -40,4 +40,5 @@ def hello():
     return "Webhook running."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
